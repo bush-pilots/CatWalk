1e1d0ea93d7cae9f5817e4e01c6605edd0aa2b50
Nick Papadakis <nickfin2014@gmail.com>
2021-03-01 10:54:01 -0800

    added automated authorization header for API helpers

--- helpers/api.js
+++ helpers/api.js
@@ -1,35 +1,37 @@
 const axios = require('axios');
 const config = require('../config.js');
 
+axios.defaults.headers.common['authorization'] = config.API_TOKEN;
+
 //broken down by widget to minimize toe-on-toe action
 
 
 //PRODUCTS DETAIL WIDGET HELPERS
 
 let amazingSampleHelper = (cb) => {
-  
+
 //(just a sample, may need revision based on how this API actually behaves)
-  
+
   let options = {
     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products?page=1&count=5`,
     headers: {
       'Authorization': `token ${config.API_TOKEN}`
     }
   };
-  
+
     axios.get(options.url, options.headers)
     .then(response => cb(null, response))
     .catch(err => cb(err, null));
 
 }
-  
+
 //RATINGS/REVIEWS WIDGET HELPERS
 
 
 
 
 
-  
+
 
 // Q/A WIDGET HELPERS
 

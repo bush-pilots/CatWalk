8e3f68224b2f02558c45f0ae6b2a6eda518ac7a2
Dennis Arnold <dennis.j.arnold.iii@gmail.com>
2021-03-01 10:10:32 -0700

    Test functions and Routing after being linted

--- helpers/api.js
+++ helpers/api.js
@@ -1,83 +1,62 @@
+/* eslint-disable max-len */
 const axios = require('axios');
 const config = require('../config.js');
 
-//broken down by widget to minimize toe-on-toe action
+// broken down by widget to minimize toe-on-toe action
 
-//Sets default authorization in header to your config.TOKEN. This should mean you do not need to add it into your requests
-axios.defaults.headers.common['authorization'] = config.API_TOKEN
+// Sets default authorization in header to your config.TOKEN. This should mean you do not need to add it into your requests
+axios.defaults.headers.common.authorization = config.API_TOKEN;
 
+// PRODUCTS DETAIL WIDGET HELPERS
 
+// eslint-disable-next-line no-unused-vars
+const amazingSampleHelper = (cb) => {
+// (just a sample, may need revision based on how this API actually behaves)
 
-//PRODUCTS DETAIL WIDGET HELPERS
-
-let amazingSampleHelper = (cb) => {
-
-//(just a sample, may need revision based on how this API actually behaves)
-
-  let options = {
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products?page=1&count=5`,
+  const options = {
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products?page=1&count=5',
     headers: {
-      'Authorization': `token ${config.API_TOKEN}`
-    }
-  }
+      Authorization: `token ${config.API_TOKEN}`,
+    },
+  };
 
-    axios.get(options.url, options.headers)
-    .then(response => cb(null, response))
-    .catch(err => cb(err, null));
+  axios.get(options.url, options.headers)
+    .then((response) => cb(null, response))
+    .catch((err) => cb(err, null));
+};
 
-}
-
-let getProductData = (id, cb) => {
+const getProductData = (id, cb) => {
   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${id}`)
-  .then((response) => {
-    cb(null, response)
-  })
-  .catch(err => cb(err, null))
-}
+    .then((response) => {
+      cb(null, response);
+    })
+    .catch((err) => cb(err, null));
+};
 
-let getStyles = (id, cb) => {
+const getStyles = (id, cb) => {
   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${id}/styles`)
-  .then((response) => {
-    cb(null, response)
-  })
-  .catch(err => cb(err, null))
-}
+    .then((response) => {
+      cb(null, response);
+    })
+    .catch((err) => cb(err, null));
+};
 
-let getRelated = (id, cb) => {
+const getRelated = (id, cb) => {
   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${id}/related`)
-  .then((response) => {
-    cb(null, response)
-  })
-  .catch(err => cb(err, null))
-}
+    .then((response) => {
+      cb(null, response);
+    })
+    .catch((err) => cb(err, null));
+};
 
-//RATINGS/REVIEWS WIDGET HELPERS
-
-
-
-
-
-
+// RATINGS/REVIEWS WIDGET HELPERS
 
 // Q/A WIDGET HELPERS
 
-
-
-
-
-
-
 // RELATED ITEMS/OUTFIT WIDGET HELPERS
 
-
-
-
-
-
 module.exports = {
-  //export all helper functions here,
-  //amazingSampleHelper,
-  //...
   getProductData,
-  getStyles
-}
+  getStyles,
+  getRelated,
+};

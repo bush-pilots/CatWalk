0a4767da4762bc48f87eeb3e3a4dd7c8142219d7
Nick Papadakis <nickfin2014@gmail.com>
2021-03-04 08:21:34 -0800

    added Modal popout with CSS

--- client/src/components/QA/QA.js
+++ client/src/components/QA/QA.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
+import { Button } from '@material-ui/core';
 import QuestionList from './QuestionList.js';
-import AddQuestionModal from './AddQuestionModal';
+import AddQuestionModal from './AddQuestionModal.js'
 
 const api = require('../../../../helpers/api');
 
@@ -14,17 +15,6 @@ const QA = ({product, id}) => {
     getQuestions();
   }, [id]);
 
-  // useEffect( () => {
-  //   getQuestions();
-  // }, [count]);
-
-  // useEffect( () => {
-  //   if(questions.length > count) {
-  //     setQuestions(questions => questions = questions.slice(0, count));
-  //     console.log('count did not update in time, but I got you fam!');
-  //   }
-  // }, [questions.length]);
-
   const getQuestions = () => {
     api.getQuestions(id, (err, results) => {
       setQuestions(results);
@@ -33,20 +23,20 @@ const QA = ({product, id}) => {
 
   const toggleModal = () => {
     setDisplayModal(!displayModal);
-  }
+  };
 
   return (
     <div id="QA">
-    <div className="QA-header">QUESTIONS and ANSWERS</div>
-    <div>Search Bar Placeholder</div>
-    <QuestionList questions={questions.slice(0, count)} />
-    {questions.length > 2 && (<button className="QA-moreQuestionsButton button" onClick={() => {
-      setCount(count + 2)}}>
-        MORE ANSWERED QUESTIONS</button>)}
-    <button onClick={() => toggleModal()} className="QA-addQuestionButton button">ADD A QUESTION +</button>
-    <AddQuestionModal product={product.name} displayModal={displayModal} toggleModal={toggleModal}/>
+      <div className="QA-header">QUESTIONS and ANSWERS</div>
+      <div>Search Bar Placeholder</div>
+      <QuestionList questions={questions.slice(0, count)} />
+      {questions.length > 2 && (<button className="QA-moreQuestionsButton button" onClick={() => {
+        setCount(count + 2)}}>
+          MORE ANSWERED QUESTIONS</button>)}
+      <Button color="primary" onClick={() => toggleModal()} className="QA-addQuestionButton button">ADD A QUESTION +</Button>
+        {displayModal && (<AddQuestionModal product={product} displayModal={displayModal} toggleModal={toggleModal}/>)}
     </div>
-  )
+  );
 };
 
 export default QA;

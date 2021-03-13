b8501efbcc0212a9b03c1dc87e61136881ad5b2e
Dennis Arnold <dennis.j.arnold.iii@gmail.com>
2021-03-13 11:31:07 -0700

    commit before pull

--- client/src/components/QA/QA.js
+++ client/src/components/QA/QA.js
@@ -42,9 +42,10 @@ const QA = ({onClick, product, id}) => {
 
         <Button onClick={() => toggleModal()} size="medium" color="primary" className="QA-AddQuestion">ADD A QUESTION +</Button>
 
-    {(count >= questions.length) && (<Button size="medium" color="primary" className="QA-CollapseQuestions small" onClick={() => setCount(2)}>Collapse Questions</Button>)}
+    {(count > 2 && count >= questions.length) && (<Button size="medium" color="primary" className="QA-CollapseQuestions small" onClick={() => setCount(2)}>Collapse Questions</Button>)}
 
-      {displayModal && (<Modal questionModal={true} question={null} product={product} displayModal={displayModal} toggleModal={toggleModal}/>)}
+      {displayModal && (<Modal questionModal={true} question={null} product={product}
+      toggleModal={toggleModal} getQuestions={getQuestions}/>)}
 
     </div>
   );
